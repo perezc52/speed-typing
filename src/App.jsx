e23c6cc23875b5146e860775d92a7f71864185c3
Carlos Perez <pcarlos132@yahoo.com>
2023-01-13 11:23:08 -0600

    finished mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-/**
- * Challenge: build the basic structure of our game
- * 
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in 
- *      (tip: React normalizes <textarea /> to be more like <input />, 
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
+
 function App() {
+  const TIME_REMAINING = 10
+
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(TIME_REMAINING)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [words, setWords] = useState(0)
+
+  useEffect(() => {
+    if(isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1)
+      }, 1000)
+    }else if (timeRemaining === 0) {
+      endGame()
+    }
+  }, [timeRemaining, isTimeRunning])
+
+  function startGame() {
+    setTimeRemaining(TIME_REMAINING)
+    setIsTimeRunning(true)
+    setText("")
+  }
+
+  function endGame() {
+    setWords(calculateNumberOfWords(text))
+    setIsTimeRunning(false)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+    setText(value)
+  }
+
+  function calculateNumberOfWords(text) {
+    console.log(text.trim().split(' ').filter(el => el !== "").length)
+    return text.trim().split(' ').filter(el => el !== "").length
+  }
+
   return (
     <div>
       <h1>Speed Typer</h1>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <h4>Time remaining:</h4>
-      <button>Start Game</button>
-      <h1>Word Count:</h1>
+      <textarea 
+        name="text"
+        id="text" 
+        cols="30" rows="10" 
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}>
+      </textarea>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button disabled={isTimeRunning} onClick={startGame}>Start Game</button>
+      <h1>Word Count: {words}</h1>
     </div>
   )
 }
